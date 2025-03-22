4380051203df569f39166728d7b72d3b8397c52b
medasani <medasanicharan66@gmail.com>
2025-03-22 10:24:19 +0530

    example

--- vpc.tf
+++ vpc.tf
@@ -128,10 +128,10 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block    = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+  # route {
+  #   cidr_block    = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.gw.id
+  # }
 
   tags = merge(
     var.common_tags,
@@ -147,10 +147,10 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.nat.id
+  # }
  
   tags = merge(
     var.common_tags,
@@ -166,10 +166,10 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   nat_gateway_id = aws_nat_gateway.nat.id
+  # }
 
   tags = merge(
     var.common_tags,
@@ -181,7 +181,7 @@ resource "aws_route_table" "database" {
 }
 
 
-/* ## public route ##
+## public route ##
 
 resource "aws_route" "public_route" {
   route_table_id            = aws_route_table.public.id
@@ -203,7 +203,7 @@ resource "aws_route" "database_route" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_nat_gateway.nat.id
-} */
+}
 
 
 ## public route table association ##
